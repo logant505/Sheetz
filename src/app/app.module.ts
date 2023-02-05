034cb4962b0393aff23082d1301adedbb33f3d11
logant505 <lat00012@mix.wvu.edu>
2023-02-05 14:21:13 -0500

    Changed Navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SideNavComponent } from './Navigation/side-nav/side-nav.component';
+import { FooterComponent } from './Navigation/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SideNavComponent
+    SideNavComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
