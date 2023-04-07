c843130dafdb76fe537b10e414a975d6a5a04c14
logant505 <lat00012@mix.wvu.edu>
2023-04-06 22:21:47 -0400

    Service File
    Removed constructor for mock list, replaced with service loop.

--- src/app/layouts/bio-layout/bio-layout.component.ts
+++ src/app/layouts/bio-layout/bio-layout.component.ts
@@ -1,18 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ProductModel } from '../card-builder/product.model';
 import { mock_list } from '../card-builder/mocklist';
+import { ProductsService } from '../card-builder/card-builder.service';
+
 
 @Component({
   selector: 'app-bio-layout',
   templateUrl: './bio-layout.component.html',
   styleUrls: ['./bio-layout.component.css']
 })
-export class BioLayoutComponent {
+export class BioLayoutComponent implements OnInit{
   products:ProductModel []=[];
-  constructor(){
-    for(var item of mock_list){
-      console.log(item);
-      this.products.push(item);
-    }
+  constructor(private productsService:ProductsService){
+
+  }
+  ngOnInit(): void {
+    this.productsService.getProducts().subscribe((data: ProductModel[])=>{
+      console.log("Fetching Products");
+      for(var product of data){
+      console.log(product);
+      this.products.push();
+      }
+    })
+    throw new Error("Method Not Implemented");
   }
 }
--- src/app/layouts/card-builder/card-builder.service.ts
+++ src/app/layouts/card-builder/card-builder.service.ts
@@ -7,7 +7,7 @@ import { ProductModel } from "./product.model";
 )
 export class ProductsService{
     private baseUrl:string="https://console.firebase.google.com/project/sheetz-6c1c5/database/sheetz-6c1c5-default-rtdb/data/~2F";
-    private productsEndPoint="Products.json";
+    private productsEndPoint="cardBuilder.json";
 
     constructor(private http:HttpClient){
 
